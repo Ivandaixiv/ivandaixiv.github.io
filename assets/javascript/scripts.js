00cb38a2bef1bbf763e03ac46b14bc28fc6329eb
Ivan Dai <ivan.bcdai@gmail.com>
2025-02-01 16:15:32 -0800

    Add a sliding navbar from button click

--- assets/javascript/scripts.js
+++ assets/javascript/scripts.js
@@ -1,3 +1,4 @@
-document.getElementById("menu-button").addEventListener("click", function() {
-    alert("Menu Clicked! You can open a sidebar here.");
+document.getElementById("menu-button").addEventListener("click", function () {
+    const navbar = document.getElementById("sliding-navbar");
+    navbar.classList.toggle("open");
 });
