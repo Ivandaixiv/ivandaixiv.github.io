d16be76b4d5aa734cce7c90d730b8dcebc45fc72
Ivan Dai <ivan.bcdai@gmail.com>
2025-02-02 21:20:07 -0800

    Add new close menu icon and animations with logic to the menu button

--- assets/javascript/scripts.js
+++ assets/javascript/scripts.js
@@ -1,4 +1,26 @@
-document.getElementById("menu-button").addEventListener("click", function () {
-    const navbar = document.getElementById("sliding-navbar");
-    navbar.classList.toggle("open");
+const menuButton = document.getElementById("menu-button");
+const openMenu = document.getElementById("open-menu");
+const closeMenu = document.getElementById("close-menu");
+const navbar = document.getElementById("sliding-navbar");
+
+menuButton.addEventListener("click", function () {
+    // const navbar = document.getElementById("sliding-navbar");
+    if(closeMenu.style.display != "none") {
+        menuButton.style.background = "#374785";
+        closeMenu.style.display = "none";
+        openMenu.style.display = "flex";
+        navbar.classList.toggle("open");
+    } else {
+        menuButton.style.background = "";
+        closeMenu.style.display = "flex";
+        openMenu.style.display = "none";
+        navbar.classList.remove("open");
+    }
+});
+
+navbar.addEventListener("click", function () {
+    menuButton.style.background = "";
+    closeMenu.style.display = "flex";
+    openMenu.style.display = "none";
+    navbar.classList.remove("open");
 });
